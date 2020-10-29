b7f1f5a0a829be282aba6285ebf933d07c1edd7f
ademola toukourou <tkradm@yahoo.fr>
2020-10-29 21:43:53 +0100

    crate flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,30 @@
 class FlatsController < ApplicationController
+
+  before_action :authenticate_user! , only:  [:new, :create]
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    @flat.user = current_user
+    if @flat.save
+      redirect_to root_path
+    else
+
+      render :new
+    end
+  end
+
+  private
+
+
+  def params_flat
+    params.require(:flat).permit(:address, :presentation, :rent, photos: [])
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,9 +2,9 @@ class Flat < ApplicationRecord
   belongs_to :user
 
   has_many :flat_equipments, dependent: :destroy
-  has_many :equipments, through: :flat_equipment
+  has_many :equipments, through: :flat_equipments
   has_many :likes, dependent: :destroy
 
   validates :user, :address, :presentation, :rent, presence: true
-
+  has_many_attached :photos
 end
