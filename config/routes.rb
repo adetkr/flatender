286e9f5d37dc99f11ed08f9e304c5a4cc455bfda
pierre claude <pierre.claude99@gmail.com>
2020-11-04 19:10:15 +0100

    user edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  # get '/sign_out' => 'devise/sessions#destroy'
   resources :flats
   resources :likes, only: [:create]
-
+  # resources :users, only: [:update, :show, :edit]
 end
