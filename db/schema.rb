5b8ee72d55db18d19359a7ce5c236efa34511d9b
aline leroy <aline.leroy.27@gmail.com>
2020-11-07 15:17:33 +0100

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_11_06_104952) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "contracts", force: :cascade do |t|
+    t.string "signature"
+    t.string "content"
+    t.bigint "match_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["match_id"], name: "index_contracts_on_match_id"
+  end
+
   create_table "equipment", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -116,6 +125,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_104952) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "contracts", "matches"
   add_foreign_key "flat_equipments", "equipment"
   add_foreign_key "flat_equipments", "flats"
   add_foreign_key "flat_matches", "flats"
