8a90d2a04f5bd3e4b4268c7bec78e14c828f1286
Alexandre Levesque <alevek.75@gmail.com>
2020-11-13 01:04:27 +0400

    wicked pdf works in local

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,29 +2,15 @@ class ContractsController < ApplicationController
 
   def pdf_generate
     @contract = Contract.find(params[:contract_id])
-    pdf_html = ActionController::Base.new.render_to_string(template: 'contracts/pdf_generate', layout: 'pdf')
+    pdf_html = ActionController::Base.new.render_to_string(template: 'contracts/pdf_generate.html.erb', layout: 'pdf.html', locals: { contract: @contract, current_user: current_user })
     pdf = WickedPdf.new.pdf_from_string(pdf_html)
     send_data pdf, filename: 'votre_contrat.pdf'
-    # FileUtils.mkdir_p "#{Rails.root}/tmp/contracts"
-    # dir = Rails.root.join('tmp', 'contracts')
-    # file = File.open(dir.join('file.pdf'), 'w+')
-    # file.binmode
-    # file.write(pdf)
   end
 
 
   def show
     @contract = Contract.find(params[:id])
-    # pdf_html = ActionController::Base.new.render_to_string(template: 'contracts/show', layout: 'pdf')
-    # pdf = WickedPdf.new.pdf_from_string(pdf_html)
-    # respond_to do |format|
-    #   format.html
-    #   format.pdf do
-    #     render pdf: "#{@contract.id}",
-    #     template: "contracts/show.html.erb",
-    #     layout: 'pdf.html'
-    #   end
-    # end
+
   end
 
   def create
