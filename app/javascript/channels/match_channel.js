a3e39d326ef85f35c12c15452abb2cd12a98ba5f
pierre claude <pierre.claude99@gmail.com>
2020-11-21 10:43:55 +0100

    front

--- app/javascript/channels/match_channel.js
+++ app/javascript/channels/match_channel.js
@@ -7,10 +7,21 @@ const initMatchCable = () => {
     consumer.subscriptions.create({ channel: "MatchChannel", id: id }, {
       received(data) {
         // called when data is broadcast in the cable
-        messagesContainer.insertAdjacentHTML('beforeend', data);
+        console.log(data);
+        messagesContainer.insertAdjacentHTML('beforeend', data.message);
+        const user_id = messagesContainer.dataset.userid;
+        console.log(user_id, data.user_id);
+        if (user_id == data.user_id) {
+          const messages = messagesContainer.querySelectorAll(".message");
+          const lastMessage = messages[messages.length-1];
+          lastMessage.classList.add("sent-message-container");
+          console.log(lastMessage);
+          const messageContent = document.querySelectorAll(".message-content");
+        }
+        window.scrollTo(0, document.body.offsetHeight);
+        const messageContent = document.getElementById('message_content');
+        messageContent.value = "";
       },
     });
   }
 }
-
-export { initMatchCable };
