0e5da42f1151ee0553e5f6e7ba6baf8a22c89277
pierre claude <pierre.claude99@gmail.com>
2020-11-22 23:23:22 +0100

    amlioration dashboard

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,14 @@
 class SearchesController < ApplicationController
     def index
       @searches = Search.where(user_id: current_user.id)
+      @matchs = []
+      @match = nil
+      current_user.flats.each do |flat|
+        flatmatchs = FlatMatch.where(flat_id: flat.id)
+        flatmatchs.each do |flatmatch|
+          @matchs << Match.find(flatmatch.match.id)
+        end
+      end
     end
 
     def create
@@ -9,7 +17,7 @@ class SearchesController < ApplicationController
       @search.save!
     end
 
-    private 
+    private
 
     def params_search
       params.require(:search).permit(:city, :min_price, :max_price, :min_surface, :max_surface, :min_rooms, :max_rooms)
